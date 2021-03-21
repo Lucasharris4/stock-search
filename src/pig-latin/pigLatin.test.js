4dd21252fbfd44dd231b32e1ea8662e8da391333
lucasharris.1440@gmail.com <lucasharris@Lucass-MacBook-Pro.local>
2021-03-21 17:20:30 -0400

    refactor tests to separate invalid and valid cases

--- src/pig-latin/pigLatin.test.js
+++ src/pig-latin/pigLatin.test.js
@@ -1,47 +1,59 @@
 import { pigLatin } from './pigLatin';
-import { englishToPigLatin } from './PigLatin';
-import assert from 'assert';
+describe('pigLatin.js', () => {
+    describe('pigLatin()', () => {
+        describe('INVALID cases', () => {
+            test('if passed null value should return empty string', () => {
+                let result = pigLatin(null);
+                expect(result).toEqual('');
+                result = pigLatin(undefined);
+                expect(result).toEqual('');
+            });
 
-test('should return string value', () => {
-    const result = pigLatin('test');
-    expect(typeof result).toEqual('string');
-});
+            test('if passed empty string should return empty string', () => {
+                let result = pigLatin('');
+                expect(result).toEqual('');
+            });
 
-test('should return esttay if passed test', () => {
-    const result = pigLatin('test');
-    expect(result).toEqual('esttay');
-});
+            test('if passed a number should return empty string', () => {
+                const result = pigLatin(12345);
+                expect(result).toEqual('');
+            });
 
-test('if passed string with consonant first should return with same consonant last followed by ay', () => {
-    const result = pigLatin('headphones');
-    expect(result).toEqual('eadphoneshay');
-});
 
-test('if passed string with vowel first should return same word followed by yay', () => {
-    const result = pigLatin('ear');
-    expect(result).toEqual('earyay');
-});
+            test('should return empty string if given all spaces', () => {
+                const result = pigLatin('      ');
+                expect(result).toEqual('');
+            });
+        });
 
-test('if passed null value should return empty string', () => {
-    let result = pigLatin(null);
-    expect(result).toEqual('');
-    result = pigLatin(undefined);
-    expect(result).toEqual('');
-});
 
-test('if passed a number should return empty string', () => {
-    const result = pigLatin(12345);
-    expect(result).toEqual('');
-});
+        describe('VALID cases', () => {
+            test('should return string value', () => {
+                const result = pigLatin('test');
+                expect(typeof result).toEqual('string');
+            });
 
-test('should behave the same if passed a capital letter', () => {
-    let result = pigLatin('HeAdPhOnEs');
-    expect(result).toEqual('eadphoneshay');
-    result = pigLatin('EaR');
-    expect(result).toEqual('earyay');
-});
+            test('should return esttay if passed test', () => {
+                const result = pigLatin('test');
+                expect(result).toEqual('esttay');
+            });
 
-test('should return empty string if given all spaces', () => {
-    const result = pigLatin('      ');
-    expect(result).toEqual('');
+            test('if passed string with consonant first should return with same consonant last followed by ay', () => {
+                const result = pigLatin('headphones');
+                expect(result).toEqual('eadphoneshay');
+            });
+
+            test('if passed string with vowel first should return same word followed by yay', () => {
+                const result = pigLatin('ear');
+                expect(result).toEqual('earyay');
+            });
+
+            test('should behave the same if passed a capital letter', () => {
+                let result = pigLatin('HeAdPhOnEs');
+                expect(result).toEqual('eadphoneshay');
+                result = pigLatin('EaR');
+                expect(result).toEqual('earyay');
+            });
+        })
+    });
 });
